7c537f934534ebcb2cd6ca9341e5b6fdfcc94d13
iiolo <alohalo98@naver.com>
2022-06-29 11:16:10 +0900

    test JAVA

---
+++ MemberDAO.java
@@ -0,0 +1,13 @@
+class MemberDAO 
+{
+	public int insert(){
+		System.out.println("등록하였습니다.");
+		return 1;
+	}
+
+	public int update(){
+		System.out.println("수정하였습니다.");
+		System.out.println("수정하였습니다.");
+		return 1;
+	}
+}
